53226c2d433b2a444b599d3e185b6ed66a2bb210
Ertan Yagmur <ertanyagmur@outlook.com>
2023-01-14 00:45:04 +0300

    iformattable

---
+++ iformattable_oop/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace iformattable_oop {
+    public class Employee : IFormattable {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+
+
+        public override string ToString() {
+            return this.ToString("F", CultureInfo.CurrentCulture);
+        }
+
+        public string ToString(string format) {
+            return this.ToString(format, CultureInfo.CurrentCulture);
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider) {
+
+            if (String.IsNullOrEmpty(format)) {
+                format = "F";
+            }else if(formatProvider == null) {
+                formatProvider = CultureInfo.CurrentCulture;
+            }
+            switch (format.ToUpper()) {
+
+                case "F":
+                    return $"{Id} {FullName}";
+                case "FN":
+                    return $"{FullName}";
+                case "LF":
+                    return $"{LastName} {FirstName}";
+                case "S":
+                    return $"{LastName} {FirstName[0]}.";
+                default:
+                    throw new FormatException(string.Format("The {0} is not defined.", format));
+
+            }
+        }
+    }
+}
---
+++ iformattable_oop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Principal;
+
+namespace iformattable_oop {
+
+    public class Program {
+        static void Main(string[] args) {
+            var emps = new List<Employee>() {
+
+                new Employee() {
+                Id = 7,
+                FirstName = "Paulo",
+                LastName = "Dybala"
+                },
+                new Employee() {
+                Id = 29,
+                FirstName = "Kai",
+                LastName = "Havertz"
+                },
+                new Employee() {
+                Id = 10,
+                FirstName = "Neymar",
+                LastName = "Junior"
+                }
+
+            };
+
+            foreach (var item in emps) {
+                //Console.WriteLine(item.ToString(""));
+                //Console.WriteLine(item.ToString("FN"));
+                //Console.WriteLine(item.ToString("LF"));
+                Console.WriteLine(item.ToString("S"));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
