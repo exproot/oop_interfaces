aea7055f869de035bd6add35a459a972e2dfec6d
Ertan Yagmur <ertanyagmur@outlook.com>
2023-01-14 15:38:32 +0300

    icomparable

---
+++ icomparable_oop/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace icomparable_oop {
+    public class Employee : IComparable {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public decimal Salary { get; set; }
+
+        public int CompareTo(object obj) {
+            var cmpr = (Employee)obj;
+            if(this.Salary < cmpr.Salary) { return -1; }
+            else if (this.Salary.Equals(cmpr.Salary)) { return 0; }
+            else { return 1; }
+
+        }
+
+        public override string ToString() {
+            return $"{Id, -5} {FullName, -15} {Salary, 10}";
+        }
+    }
+}
---
+++ icomparable_oop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace icomparable_oop {
+
+    public class Program {
+        static void Main(string[] args) {
+
+            var emps = new List<Employee>() {
+                new Employee() { Id = 1, FullName = "Kai Havertz", Salary = 800000 },
+                new Employee() { Id = 2, FullName = "Guti Hernandez", Salary = 200000 },
+                new Employee() { Id = 3, FullName = "Paulo Dybala", Salary = 100000 },
+                new Employee() { Id = 4, FullName = "Neymar Jr.", Salary = 600000 },
+                new Employee() { Id = 5, FullName = "Eden Hazard", Salary = 500000 }
+            };
+
+            emps.Sort(); // sorting elements of list by salary.
+
+            Console.WriteLine($"{"ID",-5} {"Full Name", -15} {"Salary", 10}");
+
+            foreach (var item in emps) {
+                Console.WriteLine(item);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
