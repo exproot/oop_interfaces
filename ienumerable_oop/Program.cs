aed8b47e96a183e91a16445c5c3adf735b4442a8
Ertan Yagmur <ertanyagmur@outlook.com>
2023-01-14 16:11:53 +0300

    ienumerable

---
+++ ienumerable_oop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ienumerable_oop {
+    public class Program {
+        static void Main(string[] args) {
+
+            var arr1_custom = new Array(1, 3, 5,7);
+            var arr2 = new int[] { 1, 3, 5, 7 };
+
+            //predefined
+            foreach (var item in arr2) {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(new String('-', 25));
+
+            foreach (var item in arr1_custom) {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
