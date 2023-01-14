86b241a37c30744d81d3715bb241efd94a07ad84
Ertan Yagmur <ertanyagmur@outlook.com>
2023-01-14 15:59:52 +0300

    ienumerator

---
+++ ienumerator_oop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ienumerator_oop {
+    
+    public class Product {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class Program {
+        static void Main(string[] args) {
+
+            var collection = new List<Product>() {
+                new Product() {Id =1, Name = "CPU", Price = 400},
+                new Product() {Id =2, Name = "GPU", Price = 1000},
+                new Product() {Id =3, Name = "Mobo", Price = 600},
+                new Product() {Id =4, Name = "RAM", Price = 200}
+            };
+
+            foreach (var item in collection) {
+                Console.WriteLine($"{item.Id} " + $"{item.Name} " + $"{item.Price}");
+            }
+
+            Console.WriteLine(new string('-', 25));
+
+            IEnumerator<Product> enumerator = collection.GetEnumerator();
+            while (enumerator.MoveNext()) {
+                Console.WriteLine($"{enumerator.Current.Id} " + $"{enumerator.Current.Name} "
+                    + $"{enumerator.Current.Price}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
