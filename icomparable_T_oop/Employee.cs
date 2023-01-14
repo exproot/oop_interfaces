874b37d256bfa92f0402402ceaf1495c94f66a4b
Ertan Yagmur <ertanyagmur@outlook.com>
2023-01-14 15:48:38 +0300

    icomparable_T

---
+++ icomparable_T_oop/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace icomparable_T_oop {
+    public class Employee : IComparable<Employee> {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public decimal Salary { get; set; }
+
+        public int CompareTo(Employee other) {
+
+            return this.FullName.CompareTo(other.FullName);
+            //return this.Id.CompareTo(other.Id);
+            //return this.Salary.CompareTo(other.Salary);
+        }
+
+        public override string ToString() {
+            return $"{Id,-5} {FullName,-15} {Salary,10}";
+        }
+
+    }
+}
---
+++ icomparable_T_oop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace icomparable_T_oop {
+
+    public class Program {
+        static void Main(string[] args) {
+
+            var emps = new List<Employee>() {
+                new Employee() { Id = 1, FullName = "Kai Havertz", Salary = 800000 },
+                new Employee() { Id = 2, FullName = "Guti Hernandez", Salary = 200000 },
+                new Employee() { Id = 3, FullName = "Paulo Dybala", Salary = 100000 },
+                new Employee() { Id = 4, FullName = "Neymar Jr.", Salary = 600000 },
+                new Employee() { Id = 5, FullName = "Eden Hazard", Salary = 500000 }
+            };
+
+            emps.Sort();
+
+            foreach (var item in emps) {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
